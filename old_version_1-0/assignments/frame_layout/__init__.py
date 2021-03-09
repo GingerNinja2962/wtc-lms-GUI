bf593c2aaee312a288746a186f78e908f64c85b4
GingerNinja2962 <henryswessels0099@gmail.com>
2021-03-09 12:25:28 +0200

    removed old un-used files from this branch and moved them into a dedicated branch for that version

--- old_version_1-0/assignments/frame_layout/__init__.py
+++
@@ -1,9 +0,0 @@
-print("\n - Module Imported frame_layout")
-
-from assignments.frame_layout.assignment_frames import assignment_buttons_frame
-print("   - Imported assignment_buttons_frame")
-
-from assignments.frame_layout.assignment_buttons_update import assignment_buttons_update
-print("   - Imported assignment_buttons_update")
-from assignments.frame_layout.assignment_buttons_update import reset_buttons
-print("   - Imported reset_buttons")
--- old_version_1-0/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-print("\nModule Imported settings")
-
-from settings.general_settings import general_settings
-print(" - Imported general_settings")
-from settings.theme_menu import theme_menu
-print(" - Imported theme_menu")
