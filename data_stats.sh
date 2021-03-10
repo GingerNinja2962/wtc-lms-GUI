5f179e162292031cf90fb0d06cbd442c48269897
GingerNinja2962 <henryswessels0099@gmail.com>
2021-03-10 14:16:27 +0200

    added simple lauch time stamp to logging file and improved my silly data_stats file.

--- data_stats.sh
+++ data_stats.sh
@@ -1,6 +1,8 @@
 rm -rf */*/__pycache__
 rm -rf */__pycache__
 rm -rf __pycache__
+rm -rf build/
+# rm -rf .save_data # optional
 
 # wc -l : Prints the number of lines in a file.
 # wc -w : prints the number of words in a file.
@@ -8,22 +10,46 @@ rm -rf __pycache__
 # wc -m : prints the count of characters from a file.
 # wc -L : prints only the length of the longest line in a file.
 
-echo "\t===ALL DATA STATS==="
-echo "Lines:\t\t"    $(wc -l lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
-echo "Words:\t\t"    $(wc -w lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
-echo "Characters:\t" $(wc -m lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
-echo "Bytes:\t\t"    $(wc -c lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
 
-echo "\n\n\t===PROGRAM FILES STATS==="
+echo "\t=======ALL DATA STATS======"
+if test -f "./dist/wtc-lms-GUI"; then
+    echo "Lines:\t\t"    $(wc -l */wtc-lms-GUI lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+    echo "Words:\t\t"    $(wc -w */wtc-lms-GUI lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+    echo "Characters:\t" $(wc -m */wtc-lms-GUI lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+    echo "Bytes:\t\t"    $(wc -c */wtc-lms-GUI lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+else
+    echo "Lines:\t\t"    $(wc -l lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+    echo "Words:\t\t"    $(wc -w lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+    echo "Characters:\t" $(wc -m lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+    echo "Bytes:\t\t"    $(wc -c lms */*.py */*/*.py .*/*/*.txt .*/*/.*.txt | grep -iEv ".py|.txt|.md|.sh|lms|LICENSE|.gitignore")
+fi
+
+echo "\n\n\t====PROGRAM FILES STATS===="
 echo "Lines:\t\t"    $(wc -l lms */*.py */*/*.py | grep -iEv ".py|lms")
 echo "Words:\t\t"    $(wc -w lms */*.py */*/*.py | grep -iEv ".py|lms")
 echo "Characters:\t" $(wc -m lms */*.py */*/*.py | grep -iEv ".py|lms")
 echo "Bytes:\t\t"    $(wc -c lms */*.py */*/*.py | grep -iEv ".py|lms")
 
-echo "\n\n\t===SAVE DATA STATS==="
-echo "Lines:\t\t"    $(wc -l .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
-echo "Words:\t\t"    $(wc -w .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
-echo "Characters:\t" $(wc -m .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
-echo "Bytes:\t\t"    $(wc -c .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
+echo "\n\n\t===EXECUTABLE DATA STATS==="
+if test -f "./dist/wtc-lms-GUI"; then
+    echo "Lines:\t\t"    $(wc -l */wtc-lms-GUI)
+    echo "Words:\t\t"    $(wc -w */wtc-lms-GUI)
+    echo "Characters:\t" $(wc -m */wtc-lms-GUI)
+    echo "Bytes:\t\t"    $(wc -c */wtc-lms-GUI)
+else
+    echo "\n\t       +-----------+"
+    echo "\t       | NOT FOUND |"
+    echo "\t       +-----------+"
+fi
 
-# rm -rf .save_data
+echo "\n\n\t======SAVE DATA STATS======"
+if [ -d "./.save_data" ]; then
+    echo "Lines:\t\t"    $(wc -l .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
+    echo "Words:\t\t"    $(wc -w .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
+    echo "Characters:\t" $(wc -m .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
+    echo "Bytes:\t\t"    $(wc -c .*/*/*.txt .*/*/.*.txt | grep -iEv ".txt")
+else
+    echo "\n\t       +-----------+"
+    echo "\t       | NOT FOUND |"
+    echo "\t       +-----------+\n\n"
+fi
--- logger/loggerClass.py
+++ logger/loggerClass.py
@@ -1,4 +1,5 @@
-from os import close, dup, O_WRONLY, getcwd, path, open as osOpen
+from os import close, dup, O_WRONLY, getcwd, path, write, open as osOpen
+from datetime import datetime
 
 class logger:
     def __init__(self):
@@ -15,13 +16,17 @@ class logger:
         if not path.exists(self.errFile):
             open(self.errFile, 'w+').close()
 
+        currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         self.Out = dup(1)
         close(1)
         osOpen(self.outFile, O_WRONLY)
+        write(1, b"Launch Time:\t" + str.encode(currentTime))
 
         self.Err = dup(2)
         close(2)
         osOpen(self.errFile, O_WRONLY)
+        write(2, b"Launch Time:\t" + str.encode(currentTime))
 
 
     def close(self):
